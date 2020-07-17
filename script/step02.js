4480c116510f5b7511c50366d560b00da2f193ac
Jerome Bonini <jerome.bonini@newmips.com>
2020-07-17 10:42:57 +0200

    - Useless and execution breaking timeout removed - Sequence calls back API with results

--- script/step02.js
+++ script/step02.js
@@ -1,11 +1,8 @@
-setTimeout(_ => {
-	var elem = $("label:contains('Activité principale exercée'):eq(0)")
-	function extractTextNode(element) {
-		return element.parent().contents().filter(function() {
-			return this.nodeType == Node.TEXT_NODE;
-		}).text().trim().replace(/[\n\r\t]/g, '');
-	}
-	var naf = extractTextNode($("label:contains('Activité principale exercée'):eq(0)"));
-	var siret = extractTextNode($("label:contains('Siret du siège'):eq(0)"));
-	scriptFinish({naf: naf, siret: siret});
-}, 3000);
+function extractTextNode(element) {
+	return element.parent().contents().filter(function() {
+		return this.nodeType == Node.TEXT_NODE;
+	}).text().trim().replace(/[\n\r\t]/g, '');
+}
+var naf = extractTextNode($("label:contains('Activité principale exercée'):eq(0)"));
+var siret = extractTextNode($("label:contains('Siret du siège'):eq(0)"));
+scriptFinish({naf: naf, siret: siret});
--- sequence/sequence01.js
+++ sequence/sequence01.js
@@ -2,8 +2,18 @@ module.exports = {
 	execute: (utils) => {
 		return new Promise((resolve, reject) => {
 			console.log("Sequence sends to API :");
-			console.log(utils.sessionData);
-			resolve();
+			console.log({
+				f_siret: utils.sessionData.siret,
+				f_siren: utils.sessionData.naf
+			});
+			utils.api.call({
+				url: '/api/societe/'+utils.env.id,
+				method: 'put',
+				body: {
+					f_siret: utils.sessionData.siret,
+					f_siren: utils.sessionData.naf
+				}
+			}).then(resolve).catch(reject);
 		});
 	}
 };
